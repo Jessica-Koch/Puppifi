f61e77999c7387e4b57ec95f7437a211b1c668ab
Jessica <jessicakoch136@icloud.com>
2015-05-30 21:16:59 -0600

    update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,31 @@
 class UsersController < ApplicationController 
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  def index
+    render json: User.all
+  end
+
+  def show
+    render json: @user
+  end
+
+  def create
+    @user = User.create(user_params)
+    render json: @user
+  end
+
+  def destroy
+    @user.destroy
+    head :no_content
+  end
 
   private
-  def user_params
-    params.require(:user).permit(:name, :email, :avatar)
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :avatar)
+    end
 end
 
