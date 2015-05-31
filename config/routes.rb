327bc420ac18afb00d5ad46b85393d195726cd7d
Jessica <jessicakoch136@icloud.com>
2015-05-30 21:49:41 -0600

    add routes for users and dogs

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -28,7 +28,9 @@ Rails.application.routes.draw do
   #   end
 
   # Example resource route with sub-resources:
-  #   resources :products do
+  resources :users do
+    resouces :dogs
+  end
   #     resources :comments, :sales
   #     resource :seller
   #   end
