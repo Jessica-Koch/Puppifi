6e5a319a0f4ba0e8c2f019cc89573f5aced987e7
Jessica <jessicakoch136@icloud.com>
2015-05-31 00:51:10 -0600

    edit ember config initializer

--- config/initializers/ember.rb
+++ config/initializers/ember.rb
@@ -1,3 +1,5 @@
 EmberCLI.configure do |c|
-  c.app :frontend
+  # c.app :dog-js, path: '~/gSchoolProjects/projects/dog-js'
+  c.app :dog-js, path: Rails.root.join('dog-js').to_s
+  c.app :puppifi
 end
