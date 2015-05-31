b50a36d6d746dee07b252e141d046dfa21572e16
Jessica <jessicakoch136@icloud.com>
2015-05-31 00:56:27 -0600

    add ember view

--- config/initializers/ember.rb
+++ config/initializers/ember.rb
@@ -1,5 +1,5 @@
 EmberCLI.configure do |c|
-  # c.app :dog-js, path: '~/gSchoolProjects/projects/dog-js'
-  c.app :dog-js, path: Rails.root.join('dog-js').to_s
+  # c.app :dogs, path: '~/gSchoolProjects/projects/dog-js'
+  c.app :dogs, path: Rails.root.join('dog-js').to_s
   c.app :puppifi
 end
