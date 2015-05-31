f2281e7e8e5f6f60c232722782584456eee2923f
Jessica <jessicakoch136@icloud.com>
2015-05-30 21:23:32 -0600

    update dog controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,5 +1,36 @@
 class DogsController < ApplicationController
+  before_action :set_dog, only: [:show, :update, :destroy]
+
+  def index
+    render json: Dog.all
+  end
+
+  def show 
+    render json: @dog
+  end
+
+  def create 
+    @dog = Dog.create(dog_params)
+    render json: @dog
+  end
+
+  def update
+    @dog.update(dog_params)
+    render json: @dog
+  end
+
+  def destroy
+    @dog.destroy
+    head :no_content
+  end
+
   private
-  def dog_params
-    params.require(:dog).permit(:name, :age, :gender, :user_id)
+
+    def set_dog
+      @dog = Dog.find(params[:id])
+    end
+
+    def dog_params
+      params.require(:dog).permit(:name, :age, :gender, :user_id)
+    end
 end
