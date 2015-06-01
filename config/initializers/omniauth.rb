f672dbf0e47a3801868f4a23ca2d529cf80b516b
Jessica <jessicakoch136@icloud.com>
2015-06-01 10:20:56 -0600

    add failure endpoint

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,3 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :linkedin, ENV['LINKEDIN_CLIENT_KEY'], ENV['LINKEDIN_CLIENT_SECRET']
 end
 
+OmniAuth.config.on_failure = Proc.new {|env|
+OmniAuth::FailureEndpoint.new(env).redirect_to_failure
+}
