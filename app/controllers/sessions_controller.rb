2f5654e952ecf904785b282279e04fbbaf330ef2
Jessica <jessicakoch136@icloud.com>
2015-06-01 11:08:19 -0600

    add sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController 
+  
+  def create
+    auth = request.env['omniauth.auth']
+    if User.find_by_uid(auth['uid'])
+      user = User.find_by_provider_and_uid(auth['provider'], auth['uid'])
+      session[:user_id] = user.id
+      user.update(token: env['omniauth.auth']['credentials']['token'])
+      redirect_to user_path(user), :notice => 'Sign in Successful!'
+    else
+      user = User.create_with_omniauth(auth, params)
+      session[:user_id] = user.id
+      user.update(token: env['omniauth.auth']['credentials']['token'])
+      redirect_to user_path(user) :notice => 'Signed in!'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, :notice => 'Signed Out!'
+  end
+end
