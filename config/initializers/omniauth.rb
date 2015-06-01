fbb78f21b508f59bfc73b23175aecd4e9b4f4249
Jessica <jessicakoch136@icloud.com>
2015-06-01 10:38:38 -0600

    add scope to oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :linkedin, ENV['LINKEDIN_CLIENT_KEY'], ENV['LINKEDIN_CLIENT_SECRET']
+  provider :linkedin, ENV['LINKEDIN_CLIENT_KEY'], ENV['LINKEDIN_CLIENT_SECRET'], :scope 'r_fullprofile r_emailaddress r_contactinfo'
 end
 
 OmniAuth.config.on_failure = Proc.new {|env|
