c4a3fbe21efbf92fa85226855b2d227a42be3db9
Jessica <jessicakoch136@icloud.com>
2015-05-31 20:47:20 -0600

    install faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+20.times do
+  User.create(Faker::Name.name, Faker::Internet.email)
+end
