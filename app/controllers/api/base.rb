832c4e99ba030fbe81ba1416bbdb3fe7db1ec389
Jessica <jessicakoch136@icloud.com>
2015-06-07 12:48:25 -0600

    set up communication with front end

--- app/controllers/api/base.rb
+++ app/controllers/api/base.rb
@@ -1,3 +1,7 @@
+# creating a base class for Rails app 
+# going to follow /api/v1 route versioning 
+# piggy backed controllers directlyr because of autolading from Rails 
+
 module API
   class Base < Grape::API
     mount API::V1::Base
