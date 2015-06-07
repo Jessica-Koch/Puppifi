452cc6b463fddd7bd6faf4ab2cc2b7133d3f0fdf
Jessica <jessicakoch136@icloud.com>
2015-06-06 19:10:52 -0600

    remove asset pipeline

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
- gem 'active_model_serializers', '~> 0.8.1'
+ gem 'active_model_serializers', '0.9.3'
  gem 'faker'
  gem 'omniauth'
  gem 'omniauth-linkedin-oauth2'
@@ -12,17 +12,9 @@ gem 'pg'
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
-# Use CoffeeScript for .coffee assets and views
-gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 gem 'rack-cors'
-# Use jquery as the JavaScript library
-gem 'jquery-rails'
-# Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-gem 'turbolinks'
-# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -5,39 +5,18 @@ module V1
     skip_before_action :verify_authenticity_token
 
     def create
-      if fetch_or_create_user
-        render json: user, status: :created
-      else
-        render nothing: true, status: :bad_request
-      end
-    end
-
-    private
-
-    def fetch_or_create_user
-      if linkedin_auth_code
-        find_or_create_user_from_linkedin
-      elsif token
-        User.find_by(token: token)
-      end
-    end
-
-    def find_or_create_user_from_linkedin
       linkedin_authenticator = LinkedinAuthenticator.new(linkedin_auth_code)
       user_factory = UserFactory.new(linkedin_authenticator)
       user_factory.find_or_create_user
+
+      render json: user, status: :created
     end
-        
-    def token
-      @_token ||= session_params[:token]
-    end
+
+
+    private
 
     def linkedin_auth_code 
-      @_linkedin_auth_code ||= session_params[:'linkedin-auth-code']
-      end
-
-      def session_params
-        params.permit(:'linkedin-auth-code', :token)
-      end
+      params.require(:'linkedin_auth_code')
     end
   end
+end
