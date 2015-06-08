73fb257a0facc5ddc00d059a8b9bf8092456cd9d
Jessica <jessicakoch136@icloud.com>
2015-06-08 17:15:58 -0600

    update gem

--- app/controllers/api/v1/base.rb
+++ app/controllers/api/v1/base.rb
@@ -4,7 +4,7 @@ module API
   module V1
     class Base < Grape::API
       mount API::V1::Users
-        mount API::V1::Users::Dogs
+        # mount API::V1::Users::Dogs
       # mount API::V1::AnotherResource
 
       add_swagger_documentation(
